1e1315fb1d8aa40507f73ea2ca7e5b52bd88b29c
Brett Flora <1brettf@gmail.com>
2022-02-01 17:20:26 -0500

    break out logos to appropriate lists

--- src/slot.tsx
+++ src/slot.tsx
@@ -2,131 +2,7 @@ import * as React from "react"
 import {AnimatePresence, motion} from "framer-motion"
 import {wrap} from "popmotion"
 import {useInterval} from "./use-interval"
-
-const logos = [
-    "imgs/affinity.svg",
-    "imgs/airtable.svg",
-    "imgs/amplitude.svg",
-    "imgs/api.svg",
-    "imgs/awsathena.svg",
-    "imgs/azuresql.svg",
-    "imgs/bigquery.svg",
-    "imgs/chargebee.svg",
-    "imgs/cosmosdb.svg",
-    "imgs/csv.svg",
-    "imgs/databricks.svg",
-    "imgs/dialpad.svg",
-    "imgs/dynamodb.svg",
-    "imgs/fbaudience.svg",
-    "imgs/freshdesk.svg",
-    "imgs/front.svg",
-    "imgs/gohighlevel.svg",
-    "imgs/googleads.svg",
-    "imgs/gsheets.svg",
-    "imgs/harmonic.svg",
-    "imgs/hubspot.svg",
-    "imgs/intercom.svg",
-    "imgs/iterable.svg",
-    "imgs/klaviyo.svg",
-    "imgs/marketo.svg",
-    "imgs/mongodb.svg",
-    "imgs/mysql.svg",
-    "imgs/pipedrive.svg",
-    "imgs/postgresql.svg",
-    "imgs/redshift.svg",
-    "imgs/salesforce.svg",
-    "imgs/segment.svg",
-    "imgs/shipbob.svg",
-    "imgs/smartsheet.svg",
-    "imgs/snowflake.svg",
-    "imgs/stripe.svg",
-    "imgs/synapse.svg",
-    "imgs/webhook.svg",
-    "imgs/zendesk_support.svg"
-]
-
-const sourceList = [
-    "imgs/mysql.svg",
-    "imgs/csv.svg",
-    "imgs/stripe.svg",
-    "imgs/harmonic.svg",
-    "imgs/klaviyo.svg",
-    "imgs/mongodb.svg",
-    "imgs/postgresql.svg",
-    "imgs/hubspot.svg",
-    "imgs/airtable.svg",
-    "imgs/dynamodb.svg",
-    "imgs/bigquery.svg",
-    "imgs/amplitude.svg",
-    "imgs/redshift.svg",
-    "imgs/azuresql.svg",
-    "imgs/databricks.svg",
-    "imgs/gsheets.svg",
-    "imgs/webhook.svg",
-    "imgs/front.svg",
-    "imgs/fbaudience.svg",
-    "imgs/intercom.svg",
-    "imgs/segment.svg",
-    "imgs/cosmosdb.svg",
-    "imgs/dialpad.svg",
-    "imgs/shipbob.svg",
-    "imgs/gohighlevel.svg",
-    "imgs/pipedrive.svg",
-    "imgs/awsathena.svg",
-    "imgs/chargebee.svg",
-    "imgs/iterable.svg",
-    "imgs/salesforce.svg",
-    "imgs/zendesk_support.svg",
-    "imgs/api.svg",
-    "imgs/freshdesk.svg",
-    "imgs/smartsheet.svg",
-    "imgs/synapse.svg",
-    "imgs/googleads.svg",
-    "imgs/snowflake.svg",
-    "imgs/marketo.svg",
-    "imgs/affinity.svg"
-]
-const targetList = [
-    "imgs/chargebee.svg",
-    "imgs/freshdesk.svg",
-    "imgs/amplitude.svg",
-    "imgs/synapse.svg",
-    "imgs/intercom.svg",
-    "imgs/databricks.svg",
-    "imgs/redshift.svg",
-    "imgs/fbaudience.svg",
-    "imgs/cosmosdb.svg",
-    "imgs/affinity.svg",
-    "imgs/gsheets.svg",
-    "imgs/dialpad.svg",
-    "imgs/harmonic.svg",
-    "imgs/stripe.svg",
-    "imgs/mongodb.svg",
-    "imgs/smartsheet.svg",
-    "imgs/googleads.svg",
-    "imgs/dynamodb.svg",
-    "imgs/awsathena.svg",
-    "imgs/klaviyo.svg",
-    "imgs/api.svg",
-    "imgs/zendesk_support.svg",
-    "imgs/webhook.svg",
-    "imgs/bigquery.svg",
-    "imgs/gohighlevel.svg",
-    "imgs/salesforce.svg",
-    "imgs/front.svg",
-    "imgs/pipedrive.svg",
-    "imgs/segment.svg",
-    "imgs/postgresql.svg",
-    "imgs/mysql.svg",
-    "imgs/snowflake.svg",
-    "imgs/marketo.svg",
-    "imgs/airtable.svg",
-    "imgs/shipbob.svg",
-    "imgs/csv.svg",
-    "imgs/iterable.svg",
-    "imgs/azuresql.svg",
-    "imgs/hubspot.svg"
-]
+import {destination, source} from "./data"
 
 const TRAVEL = 72 // in px
 const TRAVEL_SCALE = 0.125 // % as decimal
@@ -141,7 +17,7 @@ interface SlotProps {
 export function Slot({delay, spin, hover, setHover}: SlotProps) {
     const [page, setPage] = React.useState(0)
     const direction = spin === "up" ? -1 : 1
-    const logos = spin === "up" ? targetList : sourceList
+    const logos = spin === "up" ? destination : source
 
     const imageIndex = wrap(0, logos.length, page)
 
