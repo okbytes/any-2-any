38ce614bf33c4f6188c5663f08b405c358f81538
Brett Flora <1brettf@gmail.com>
2022-02-02 12:22:14 -0500

    break out hero component

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,6 @@
-import * as React from "react"
-import {Slot} from "./slot"
-import {useInterval} from "./use-interval"
-
-const h1Styles = "text-7xl font-bold pointer-events-none"
-
-const ITEMS_IN_CYCLE = 4
-const MS_ITEM = 250 // in ms
-const MS_CYCLE = MS_ITEM * ITEMS_IN_CYCLE // in ms
-const MS_PAUSE = 4000 // in ms
-
-const PAUSE = {item: null, main: MS_PAUSE, type: "pause"}
-const CYCLE = {item: MS_ITEM, main: MS_CYCLE, type: "cycle"}
+import {Hero} from "./hero"
 
 export default function App() {
-    const [delay, setDelay] = React.useState<typeof PAUSE | typeof CYCLE>(CYCLE)
-    const [hover, setHover] = React.useState(false)
-
-    React.useEffect(() => {
-        if (hover) {
-            setDelay(PAUSE)
-        }
-    }, [hover])
-
-    useInterval(
-        () => {
-            if (delay.type === "cycle") {
-                setDelay(PAUSE)
-            } else {
-                setDelay(CYCLE)
-            }
-        },
-        hover ? null : delay.main
-    )
-
     return (
         <>
             <div className="px-10 mx-auto max-w-5xl">
@@ -71,15 +39,7 @@ export default function App() {
                     </div>
                 </nav>
                 <div className="font-sans">
-                    <div className="w-full flex items-center">
-                        <h1 className={h1Styles}>Sync</h1>
-
-                        <Slot spin="down" delay={delay.item} hover={hover} setHover={setHover} />
-
-                        <h1 className={h1Styles}>to</h1>
-
-                        <Slot spin="up" delay={delay.item} hover={hover} setHover={setHover} />
-                    </div>
+                    <Hero />
 
                     <h2 className="mt-6 text-4xl text-gray-300 font-medium">
                         The one platform to sync any data anywhere.
---
+++ src/hero.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+import {Slot} from "./slot"
+import {useInterval} from "./use-interval"
+
+const h1Styles = "text-7xl font-bold pointer-events-none"
+
+const ITEMS_IN_CYCLE = 4
+const MS_ITEM = 250 // in ms
+const MS_CYCLE = MS_ITEM * ITEMS_IN_CYCLE // in ms
+const MS_PAUSE = 4000 // in ms
+
+const PAUSE = {item: null, main: MS_PAUSE, type: "pause"}
+const CYCLE = {item: MS_ITEM, main: MS_CYCLE, type: "cycle"}
+
+export function Hero() {
+    const [delay, setDelay] = React.useState<typeof PAUSE | typeof CYCLE>(CYCLE)
+    const [hover, setHover] = React.useState(false)
+
+    React.useEffect(() => {
+        if (hover) {
+            setDelay(PAUSE)
+        }
+    }, [hover])
+
+    useInterval(
+        () => {
+            if (delay.type === "cycle") {
+                setDelay(PAUSE)
+            } else {
+                setDelay(CYCLE)
+            }
+        },
+        hover ? null : delay.main
+    )
+    return (
+        <div className="w-full flex items-center">
+            <h1 className={h1Styles}>Sync</h1>
+
+            <Slot spin="down" delay={delay.item} hover={hover} setHover={setHover} />
+
+            <h1 className={h1Styles}>to</h1>
+
+            <Slot spin="up" delay={delay.item} hover={hover} setHover={setHover} />
+        </div>
+    )
+}
